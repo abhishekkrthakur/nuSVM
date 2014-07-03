6fe5341fe945c01b34849f6a489e4f19c122761f
abhishek thakur <abhishek4@gmail.com>
2014-07-03 10:52:43 +0200

    pep8 compliance

--- nusvm.py
+++ nusvm.py
@@ -10,18 +10,22 @@ import cvxopt.solvers
 import numpy as np
 from numpy import linalg
 
+
 def linear_kernel(x1, x2):
     return np.dot(x1, x2)
+
 
 def polynomial_kernel(x, y, p=3):
     return (1 + np.dot(x, y)) ** p
 
+
 def gaussian_kernel(x, y, sigma=5.0):
-    return np.exp(-linalg.norm(x-y)**2 / (2 * (sigma ** 2)))
+    return np.exp(-linalg.norm(x - y) ** 2 / (2 * (sigma ** 2)))
+
 
 class nuSVM(object):
 
-    def __init__(self, kernel=linear_kernel, nu=None, verbose = False, threshold = 1e-5):
+    def __init__(self, kernel=linear_kernel, nu=None, verbose=False, threshold=1e-5):
         self.kernel = kernel
         self.nu = float(nu)
         self.verbose = verbose
@@ -33,16 +37,16 @@ class nuSVM(object):
         unique_labels = np.unique(y)
         assert len(unique_labels) == 2
         # Gram matrix
-        #print X
+        # print X
         K = np.zeros((n_samples, n_samples))
         for i in range(n_samples):
             for j in range(n_samples):
-                K[i,j] = self.kernel(X[i], X[j])
+                K[i, j] = self.kernel(X[i], X[j])
 
-        P = cvxopt.matrix(np.outer(y,y) * K)
+        P = cvxopt.matrix(np.outer(y, y) * K)
 
         q = cvxopt.matrix(np.ones(n_samples) * -1)
-        A = cvxopt.matrix(y, (1,n_samples))
+        A = cvxopt.matrix(y, (1, n_samples))
         b = cvxopt.matrix(0.0)
 
         tmp1 = np.diag(np.ones(n_samples) * -1)
@@ -50,12 +54,13 @@ class nuSVM(object):
         tmp3 = np.ones((1, n_samples))
         G = cvxopt.matrix(np.vstack((tmp1, tmp2, tmp3)))
         tmp1 = np.zeros(n_samples)
-        tmp2 = np.ones(n_samples) * 1./n_samples
+        tmp2 = np.ones(n_samples) * 1. / n_samples
         tmp3 = self.nu
         h = cvxopt.matrix(np.hstack((tmp1, tmp2, tmp3)))
 
         solution = cvxopt.solvers.qp(P, q, G, h, A, b)
-        if self.verbose: "nu =", self.nu
+        if self.verbose:
+            "nu =", self.nu
 
         # Lagrange multipliers
         a = np.ravel(solution['x'])
@@ -66,13 +71,14 @@ class nuSVM(object):
         self.a = a[sv]
         self.sv = X[sv]
         self.sv_y = y[sv]
-        if self.verbose: print "%d support vectors out of %d points" % (len(self.a), n_samples)
+        if self.verbose:
+            print "%d support vectors out of %d points" % (len(self.a), n_samples)
 
         # Intercept
         self.b = 0
         for n in range(len(self.a)):
             self.b += self.sv_y[n]
-            self.b -= np.sum(self.a * self.sv_y * K[ind[n],sv])
+            self.b -= np.sum(self.a * self.sv_y * K[ind[n], sv])
         self.b /= len(self.a)
 
         # Weight vector
